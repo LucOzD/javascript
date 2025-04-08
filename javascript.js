3a272e581d1bf210c253302de9a473e8bab8241b
LucOzD <71006735+LucOzD@users.noreply.github.com>
2025-04-08 15:53:35 +1000

    NEZHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH

--- javascript.js
+++ javascript.js
@@ -5,7 +5,7 @@ const myParagraph = document.getElementById("myParagraph");
 
 myButton.addEventListener("click", function() {
   if (myParagraph.textContent === "Click the button to change this text!") {
-    myParagraph.textContent = "The text has been changed!";
+    myParagraph.textContent = "AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH NEZHHHHHHHHHHHHHHHHHHHHHHHHHHH";
   } else {
     myParagraph.textContent = "Click the button to change this text!";
   }
