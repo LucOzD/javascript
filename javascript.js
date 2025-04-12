6b6d40a1455d6a2064a5d5ac6bd6f94513a4c967
LucOzD <71006735+LucOzD@users.noreply.github.com>
2025-04-12 21:22:45 +1000

    ok so javascript is cursed and i cant use it

--- javascript.js
+++ javascript.js
@@ -1,46 +1,51 @@
-
+function getPixelValue(value) {
+  return parseInt(value.replace("px", "")) || 0;
+}
 
 function leftArrowPressed() {
-var element = document.getElementById("image1");
-element.style.left = parseInt(element.style.left) - 5 + 'px';
+  var element = document.getElementById("image1");
+  var left = getPixelValue(window.getComputedStyle(element).left);
+  element.style.left = (left - 5) + "px";
 }
 
 function rightArrowPressed() {
-var element = document.getElementById("image1");
-element.style.left = parseInt(element.style.left) + 5 + 'px';
-
+  var element = document.getElementById("image1");
+  var left = getPixelValue(window.getComputedStyle(element).left);
+  element.style.left = (left + 5) + "px";
 }
 
 function upArrowPressed() {
-var element = document.getElementById("image1");
-element.style.top = parseInt(element.style.top) - 5 + 'px';
+  var element = document.getElementById("image1");
+  var top = getPixelValue(window.getComputedStyle(element).top);
+  element.style.top = (top - 5) + "px";
 }
 
 function downArrowPressed() {
-var element = document.getElementById("image1");
-element.style.top = parseInt(element.style.top) + 5 + 'px';
+  var element = document.getElementById("image1");
+  var top = getPixelValue(window.getComputedStyle(element).top);
+  element.style.top = (top + 5) + "px";
 }
 
 function moveSelection(evt) {
-    switch (evt.keyCode) {
-        case 37:
-        leftArrowPressed();
-        break;
-        case 39:
-        rightArrowPressed();
-        break;
-        case 38:
-        upArrowPressed();
-        break;
-        case 40:
-        downArrowPressed();
-        break;
-        }
-    };
-
-function docReady()
-{
-
-window.addEventListener('keydown', moveSelection);
+  switch (evt.key) {
+      case "ArrowLeft":
+          leftArrowPressed();
+          break;
+      case "ArrowRight":
+          rightArrowPressed();
+          break;
+      case "ArrowUp":
+          upArrowPressed();
+          break;
+      case "ArrowDown":
+          downArrowPressed();
+          break;
+  }
 }
 
+function skibidi() {
+  window.addEventListener('keydown', moveSelection);
+}
+
+// Run the setup
+window.onload = skibidi;
