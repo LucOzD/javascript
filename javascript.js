3a09f20603207ada98e04589635022ad4845a29f
LucOzD <71006735+LucOzD@users.noreply.github.com>
2025-04-12 20:31:53 +1000

    woah i stole a bunch of code from some guy on the internet im so good at coding

--- javascript.js
+++ javascript.js
@@ -1,12 +1,63 @@
 // script.js
 
-const myButton = document.getElementById("myButton");
-const myParagraph = document.getElementById("myParagraph");
+function leftArrowPressed(){
+  var element = document.getElementById ("image1");
+  element.style.left = parseInt(element.style.left) - 5 + "px";
+  
+  
+  };
 
-myButton.addEventListener("click", function() {
-  if (myParagraph.textContent === "Click the button to change this text!") {
-    myParagraph.textContent = "AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH NEZHHHHHHHHHHHHHHHHHHHHHHHHHHH";
-  } else {
-    myParagraph.textContent = "Click the button to change this text!";
-  }
-});
+
+  function rightArrowPressed(){
+var element = document.getElementById ("image1");
+element.style.left = parseInt(element.style.left) + 5 + "px";
+
+
+};
+
+function upArrowPressed(){
+  var element = document.getElementById ("image1");
+  element.style.left = parseInt(element.style.top) - 5 + "px";
+  
+  
+  };
+
+  function downArrowPressed(){
+    var element = document.getElementById ("image1");
+    element.style.left = parseInt(element.style.top) + 5 + "px";
+    
+    
+    };
+
+
+
+
+function moveSelection(evt) {
+switch (evt.keyCode) {
+case 37:
+  leftArrowPressed();
+  break;
+
+case 39:
+  rightArrowPressed();
+  break;
+
+  case 38:
+    upArrowPressed();
+    break;
+
+    case 40:
+  downArrowPressed();
+  break;
+};
+
+function docReady(){
+
+window.addEventListener('keydown',
+  moveSelection)
+
+
+}
+
+
+}
