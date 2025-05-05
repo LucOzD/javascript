ccd2e15dbdc4f5aef0909ffdf9a04d401b2b4d49
LucOzD <71006735+LucOzD@users.noreply.github.com>
2025-05-05 13:32:55 +1000

    "lenth" 🥵 im skibidi stupid

--- javascript.js
+++ javascript.js
@@ -1,9 +1,11 @@
-var myVariable = ["i.. am steve","chicken jockey","ender pearl", "this... is a crafting table", "diamond armour, full set","blades for days"];
+var myVariable = ["i.. am steve", "chicken jockey", "ender pearl", "this... is a crafting table", "diamond armour, full set", "blades for days"];
 var words = "what the sigma";
-function buttonPressed(){
- words = myVariable[Math.floor(Math.random()*myVariable.lenth)];
-document.getElementById("myParagraph").textContent = words;
 
+function buttonPressed() {
+  // Correct the typo here: .length
+  var randomIndex = Math.floor(Math.random() * myVariable.length);
+  words = myVariable[randomIndex];
+  document.getElementById("myParagraph").textContent = words;
 }
 
 
