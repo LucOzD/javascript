4b0a7aab58231d981525ee7cd93860f823de23ae
LucOzD <71006735+LucOzD@users.noreply.github.com>
2025-05-05 13:25:06 +1000

    im actually insane

--- javascript.js
+++ javascript.js
@@ -1,8 +1,8 @@
-var myVariable = 0;
+var myVariable = ["i.. am steve","chicken jockey","ender pearl", "this... is a crafting table", "diamond armour, full set","blades for days"];
 
 function buttonPressed(){
-myVariable = myVariable + 1;
-document.getElementById("myParagraph").textContent = myVariable;
+var words = myVariable[Math.floor(Math.random()*myVariable.legnth)]
+document.getElementById("myParagraph").textContent = words;
 
 }
 
